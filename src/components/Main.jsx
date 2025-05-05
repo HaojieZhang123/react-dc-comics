85348d41810cb3120b680be818cc73f8e661408d
HaojieZhang123 <haojiezhang123@gmail.com>
2025-05-05 15:08:27 +0200

    [UPD] Main styling done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,15 @@ const Main = () => {
     return (
         <>
             <main>
-                <div className="container">
-                    <h2>--&gt; Content goes here &lt;--</h2>
+                <div className="bg-black">
+                    <div className="container content">
+                        <h2>--&gt; Content goes here &lt;--</h2>
+                    </div>
                 </div>
-                <div className="container">
-                    Componente aggiuntivo bonus
+                <div className="bg-blue">
+                    <div className="container content">
+                        Componente aggiuntivo bonus
+                    </div>
                 </div>
             </main>
         </>
