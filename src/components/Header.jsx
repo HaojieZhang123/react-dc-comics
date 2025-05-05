3b36a9ec41b92b7ae5ba76eab89ba2c777f87a6a
HaojieZhang123 <haojiezhang123@gmail.com>
2025-05-05 14:51:18 +0200

    [UPD] Header structure done

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import logo from '../assets/img/dc-logo.png'
+
+const Header = () => {
+    return (
+        <>
+            <header className='container'>
+                <div>
+                    <figure>
+                        <img src={logo} alt="" />
+                    </figure>
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+                    </ul>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
