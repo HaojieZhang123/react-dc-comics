f90a7de83fd2205386524a964a8522d737447012
HaojieZhang123 <haojiezhang123@gmail.com>
2025-05-05 15:04:08 +0200

    [UPD] Main structure done

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+const Main = () => {
+    return (
+        <>
+            <main>
+                <div className="container">
+                    <h2>--&gt; Content goes here &lt;--</h2>
+                </div>
+                <div className="container">
+                    Componente aggiuntivo bonus
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Main
