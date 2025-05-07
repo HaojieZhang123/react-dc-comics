ecc8a4fb07f667b3cc0cc3046e87aa1be1a0a21b
HaojieZhang123 <haojiezhang123@gmail.com>
2025-05-07 16:12:40 +0200

    [UPD] added jumbotron and section title to the comics cards' section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,17 @@
 import MainButtons from "./MainButtons"
 import ListComics from "./ListComics"
+import HeroBanner from "../assets/img/jumbotron.jpg"
+
 const Main = () => {
     return (
         <>
             <main>
+                <div className="jumbotron">
+                    <img src={HeroBanner} alt="Hero banner" />
+                </div>
                 <div className="bg-black">
-                    <div className="container content">
+                    <div className="container content position-relative">
+                        <div className="section-title">Current series</div>
                         <ListComics />
                     </div>
                 </div>
